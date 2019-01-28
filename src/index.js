471872bd9f8b18618b3d67ec6051a4723d8885f5
hstamenov3 <hristo.stamenov3@gmail.com>
2019-01-28 08:28:58 +0200

    Revert "fix swiper pagination location"
    This reverts commit b0090df765ec72ea67ded5d783191c253c549a0d.

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ const styles = {
   },
 
   slide: {
+    alignSelf: 'center',
     backgroundColor: 'transparent',
   },
 
@@ -95,8 +96,6 @@ const styles = {
     fontFamily: 'Arial'
   }
 }
-
-const window = Dimensions.get('window')
 
 // missing `module.exports = exports['default'];` with babel6
 // export default React.createClass({
@@ -215,87 +214,54 @@ export default class extends Component {
   }
 
   initState (props, updateIndex = false) {
-    // new state
+    // set the current state
+    const state = this.state || { width: 0, height: 0, offset: { x: 0, y: 0 } }
+
     const initState = {
       autoplayEnd: false,
       loopJump: false,
-      offset: {x: 0, y: 0}
+      offset: {}
     }
-
-    //-----------------------------------------------------
-    // set total (new number of children)
-    //-----------------------------------------------------
 
     initState.total = props.children ? props.children.length || 1 : 0
 
-    //-----------------------------------------------------
-    // set index (its values are 0..total-1)
-    //-----------------------------------------------------
-
-    if (this.state && this.state.total === initState.total && !updateIndex) {
+    if (state.total === initState.total && !updateIndex) {
       // retain the index
-      initState.index = this.state.index
+      initState.index = state.index
     } else {
-      // correct index in case some children were removed
-      initState.index = initState.total > 1
-        ? Math.min(props.index, initState.total - 1)
-        : 0
+      initState.index = initState.total > 1 ? Math.min(props.index, initState.total - 1) : 0
     }
 
-    //-----------------------------------------------------
-    // set direction
-    //-----------------------------------------------------
-
+    // Default: horizontal
+    let { width, height } = Dimensions.get('window')
+    width = this.props.slideWidth || width
     initState.dir = props.horizontal === false ? 'y' : 'x'
 
-    //-----------------------------------------------------
-    // set width
-    //-----------------------------------------------------
-
     if (props.width) {
-      initState.width = props.width
-    } else if (this.state && this.state.width) {
-      initState.width = this.state.width
+      initState.width = props.slideWidth || props.width
+    } else if (this.state && this.state.width){
+      initState.width = props.slideWidth || this.state.width
     } else {
-      initState.width = window.width
+      initState.width = props.slideWidth || width;
     }
-
-    //-----------------------------------------------------
-    // set height
-    //-----------------------------------------------------
 
     if (props.height) {
       initState.height = props.height
-    } else if (this.state && this.state.height) {
+    } else if (this.state && this.state.height){
       initState.height = this.state.height
     } else {
-      initState.height = window.height
+      initState.height = height;
     }
 
-    //-----------------------------------------------------
-    // set offset
-    //-----------------------------------------------------
+    initState.offset[initState.dir] = initState.dir === 'y'
+      ? height * props.index
+      : props.slideWidth ? props.slideWidth * props.index : width * props.index
 
-    const loopIndex = this.props.loop
-      ? initState.index + 1
-      : initState.index
-
-    if (initState.dir === 'x') {
-      initState.offset.x = initState.width * loopIndex
-    } else {
-      initState.offset.y = initState.height * loopIndex
-    }
-
-    //-----------------------------------------------------
-    // set internals
-    //-----------------------------------------------------
 
     this.internals = {
       ...this.internals,
-      offset: initState.offset,
       isScrolling: false
     };
-
     return initState
   }
 
@@ -305,9 +271,9 @@ export default class extends Component {
   }
 
   onLayout = (event) => {
-    const {width, height} = event.nativeEvent.layout
+    const { width, height } = event.nativeEvent.layout
     const offset = this.internals.offset = {}
-    const state = {width, height}
+    const state = {width: this.props.slideWidth || width, height}
 
     if (this.state.total > 1) {
       let setup = this.state.index
@@ -316,7 +282,7 @@ export default class extends Component {
       }
       offset[this.state.dir] = this.state.dir === 'y'
         ? height * setup
-        : width * setup
+        : this.props.slideWidth ? this.props.slideWidth * setup : width * setup
     }
 
     // only update the offset in state if needed, updating offset while swiping
@@ -385,11 +351,10 @@ export default class extends Component {
   onScrollEnd = e => {
     // update scroll state
     this.internals.isScrolling = false
-
     // making our events coming from android compatible to updateIndex logic
     if (!e.nativeEvent.contentOffset) {
       if (this.state.dir === 'x') {
-        e.nativeEvent.contentOffset = {x: e.nativeEvent.position * this.state.width}
+        e.nativeEvent.contentOffset = {x: e.nativeEvent.position * this.props.slideWidth || this.state.width}
       } else {
         e.nativeEvent.contentOffset = {y: e.nativeEvent.position * this.state.height}
       }
@@ -428,9 +393,12 @@ export default class extends Component {
    * @param  {string} dir    'x' || 'y'
    */
   updateIndex = (offset, dir, cb) => {
-    let index = this.state.index
+    const state = this.state
+    let index = state.index
+    if (!this.internals.offset)   // Android not setting this onLayout first? https://github.com/leecade/react-native-swiper/issues/582
+      this.internals.offset = {}
     const diff = offset[dir] - this.internals.offset[dir]
-    const step = dir === 'x' ? this.state.width : this.state.height
+    const step = dir === 'x' ? this.props.slideWidth || state.width : state.height
     let loopJump = false
 
     // Do nothing if offset no change.
@@ -443,10 +411,10 @@ export default class extends Component {
 
     if (this.props.loop) {
       if (index <= -1) {
-        index = this.state.total - 1
-        offset[dir] = step * this.state.total
+        index = state.total - 1
+        offset[dir] = step * state.total
         loopJump = true
-      } else if (index >= this.state.total) {
+      } else if (index >= state.total) {
         index = 0
         offset[dir] = step
         loopJump = true
@@ -493,13 +461,14 @@ export default class extends Component {
     const diff = (this.props.loop ? 1 : 0) + index + this.state.index
     let x = 0
     let y = 0
-    if (state.dir === 'x') x = diff * state.width
+    if (state.dir === 'x') x = diff * this.props.slideWidth || state.width
     if (state.dir === 'y') y = diff * state.height
 
     if (Platform.OS !== 'ios') {
       this.scrollView && this.scrollView[animated ? 'setPage' : 'setPageWithoutAnimation'](diff)
     } else {
-      this.scrollView && this.scrollView.scrollTo({ x, y, animated })
+      console.warn(diff * this.props.slideWidth)
+      this.scrollView && this.scrollView.scrollTo({x, y, animated})
     }
 
     // update scroll state
@@ -653,6 +622,7 @@ export default class extends Component {
   refScrollView = view => {
     this.scrollView = view;
   }
+
   onPageScrollStateChanged = state => {
     switch (state) {
       case 'dragging':
@@ -715,7 +685,7 @@ export default class extends Component {
       loadMinimalLoader,
       renderPagination,
       showsButtons,
-      showsPagination,
+      showsPagination
     } = this.props;
     // let dir = state.dir
     // let key = 0
